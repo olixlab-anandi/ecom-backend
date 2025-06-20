78a4b5e85ec7e458dfeece1d9d4f291b601c915c
olixlab-anandi <olixlab.50@gmail.com>
2025-06-20 18:06:05 +0530

    'chages'

--- config/db.ts
+++ config/db.ts
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 const connection = () => {
   try {
-    mongoose.connect(process.env.MONGO_URL as string);
-    console.log("Database Connected");
+    mongoose
+      .connect(process.env.MONGO_URL as string)
+      .then(() => {
+        console.log("Database Connected");
+      })
+      .catch((error) => {
+        console.log("Error in Database Connection", error);
+      });
   } catch (error) {
     console.log("Error in Connection", error);
   }
 };
 
-export default connection
+export default connection;
--- server.ts
+++ server.ts
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: process.env.ORIGIN || "http://localhost:5173",
+    origin: process.env.ORIGIN || "*",
     credentials: true,
   })
 );
